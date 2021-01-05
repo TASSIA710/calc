1b9fcab8a1af6b221b517ca9e9cc50c9db4737aa
Tassilo <tassia710@gmail.com>
2021-01-05 03:44:50 +0100

    Added JavaDoc

--- src/main/java/net/tassia/calc/value/ConstantValue.java
+++ src/main/java/net/tassia/calc/value/ConstantValue.java
@@ -1,14 +1,27 @@
 package net.tassia.calc.value;
 
+/**
+ * A constant value has a fixed double associated with it.
+ * @author Tassilo
+ * @since Calc 1.0
+ */
 public class ConstantValue implements NumericValue {
 
     private final double value;
 
-    public ConstantValue(double value) {
+	/**
+	 * Creates a new constant value with the given value.
+	 * @param value the value
+	 */
+	public ConstantValue(double value) {
         this.value = value;
     }
 
-    public double getValue() {
+	/**
+	 * Returns the value of this constant value.
+	 * @return the value
+	 */
+	public double getValue() {
         return value;
     }
 
