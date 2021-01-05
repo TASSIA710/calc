a674787ba7a8325e709bf6fb27548ca84eb03c6c
Tassilo <tassia710@gmail.com>
2021-01-05 03:44:48 +0100

    Added JavaDoc

--- src/main/java/net/tassia/calc/value/BinaryValue.java
+++ src/main/java/net/tassia/calc/value/BinaryValue.java
@@ -2,26 +2,49 @@ package net.tassia.calc.value;
 
 import net.tassia.calc.Operator;
 
+/**
+ * A binary value consists of an operator and two numeric values.
+ * @author Tassilo
+ * @since Calc 1.0
+ */
 public class BinaryValue implements NumericValue {
 
     private final Operator.Binary operator;
     private final NumericValue leftHand, rightHand;
 
+	/**
+	 * Creates a new binary value with the given operator, left-hand and right-hand value.
+	 * @param operator the operator
+	 * @param leftHand the left-hand value
+	 * @param rightHand the right-hand value
+	 */
     public BinaryValue(Operator.Binary operator, NumericValue leftHand, NumericValue rightHand) {
         this.operator = operator;
         this.leftHand = leftHand;
         this.rightHand = rightHand;
     }
 
-    public Operator.Binary getOperator() {
+	/**
+	 * Returns the operator of this binary value.
+	 * @return the operator
+	 */
+	public Operator.Binary getOperator() {
         return operator;
     }
 
-    public NumericValue getLeftHand() {
+	/**
+	 * Returns the left-hand value of this binary value.
+	 * @return the left-hand value
+	 */
+	public NumericValue getLeftHand() {
         return leftHand;
     }
 
-    public NumericValue getRightHand() {
+	/**
+	 * Returns the right-hand value of this binary value.
+	 * @return the right-hand value
+	 */
+	public NumericValue getRightHand() {
         return rightHand;
     }
 
