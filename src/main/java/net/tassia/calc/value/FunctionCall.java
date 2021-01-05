2f77306a477b3fe3032b2a45304a1cce01dac2b4
Tassilo <tassia710@gmail.com>
2021-01-05 03:44:56 +0100

    Added JavaDoc

--- src/main/java/net/tassia/calc/value/FunctionCall.java
+++ src/main/java/net/tassia/calc/value/FunctionCall.java
@@ -1,20 +1,38 @@
 package net.tassia.calc.value;
 
+/**
+ * A function call represents a call to a function.
+ * @author Tassilo
+ * @since Calc 1.0
+ */
 public class FunctionCall implements NumericValue {
 
     private final String function;
     private final NumericValue[] arguments;
 
+	/**
+	 * Creates a new function call with the given function name and arguments.
+	 * @param function the name
+	 * @param arguments the arguments
+	 */
     public FunctionCall(String function, NumericValue...arguments) {
         this.function = function;
         this.arguments = arguments;
     }
 
-    public String getFunction() {
+	/**
+	 * Returns the name of the called function.
+	 * @return the name
+	 */
+	public String getFunction() {
         return function;
     }
 
-    public NumericValue[] getArguments() {
+	/**
+	 * Returns the arguments of this function call.
+	 * @return the arguments
+	 */
+	public NumericValue[] getArguments() {
         return arguments;
     }
 
