73776095bc300d520fe6451cf67ac701ad1933d0
Tassilo <tassia710@gmail.com>
2021-01-05 01:21:51 +0100

    Improved test

--- src/test/java/net/tassia/calculator/CalculatorTest.java
+++ src/test/java/net/tassia/calculator/CalculatorTest.java
@@ -5,14 +5,23 @@ import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
 
-	@Test
-	void calculate() throws ParseException {
+	void testExpression(String term, double expect) throws ParseException {
 		try {
-			assert Calculator.calculate("2 * 2") == 4;
+			double result = Calculator.calculate(term);
+			System.out.println(term + " = " + result);
+			assert result == expect;
 		} catch (ParseException ex) {
 			System.out.println(ParseException.getDisplayed(ex));
 			throw ex;
 		}
+	}
+
+	@Test
+	void calculate() throws ParseException {
+		testExpression("2 + 2", 4);
+		testExpression("2 * 2", 4);
+		testExpression("max(1, 2, 3, 4, 5)", 5);
+		testExpression("min(1, 2, 3, 4, 5)", 1);
 	}
 
 }
