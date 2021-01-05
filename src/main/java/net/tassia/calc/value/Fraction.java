90abef61f2b6b47fec2cec4afdf3d50b780dae86
Tassilo <tassia710@gmail.com>
2021-01-05 03:44:53 +0100

    Added JavaDoc

--- src/main/java/net/tassia/calc/value/Fraction.java
+++ src/main/java/net/tassia/calc/value/Fraction.java
@@ -1,19 +1,37 @@
 package net.tassia.calc.value;
 
+/**
+ * A fraction consists of an upper and a lower part.
+ * @author Tassilo
+ * @since Calc 1.0
+ */
 public class Fraction implements NumericValue {
 
     private final NumericValue upper, lower;
 
-    public Fraction(NumericValue upper, NumericValue lower) {
+	/**
+	 * Creates a new fraction with the given upper and lower parts.
+	 * @param upper the upper part
+	 * @param lower the lower part
+	 */
+	public Fraction(NumericValue upper, NumericValue lower) {
         this.upper = upper;
         this.lower = lower;
     }
 
-    public NumericValue getUpper() {
+	/**
+	 * Returns the upper part of this fraction.
+	 * @return the upper part
+	 */
+	public NumericValue getUpper() {
         return upper;
     }
 
-    public NumericValue getLower() {
+	/**
+	 * Returns the lower part of this fraction.
+	 * @return the lower part
+	 */
+	public NumericValue getLower() {
         return lower;
     }
 
