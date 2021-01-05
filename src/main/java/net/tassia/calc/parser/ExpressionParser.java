a8ac32f8ba8dba47a1f58ebefa9c8275deb4a09d
Tassilo <tassia710@gmail.com>
2021-01-05 01:37:15 +0100

    Added JavaDoc

--- src/main/java/net/tassia/calc/parser/ExpressionParser.java
+++ src/main/java/net/tassia/calc/parser/ExpressionParser.java
@@ -2,8 +2,19 @@ package net.tassia.calc.parser;
 
 import net.tassia.calc.value.NumericValue;
 
+/**
+ * Implementing classes can parse input terms into a numeric value.
+ * @author Tassilo
+ * @since Calc 1.0
+ */
 public interface ExpressionParser {
 
-	NumericValue parse(String input) throws ParseException;
+	/**
+	 * Parses the given term into a numeric value.
+	 * @param term the term
+	 * @return the numeric value
+	 * @throws ParseException if the string doesn't fit the term grammar
+	 */
+	NumericValue parse(String term) throws ParseException;
 
 }
