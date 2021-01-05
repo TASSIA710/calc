b75f0fc170af6262ff58f0720c9309dd8e008b96
Tassilo <tassia710@gmail.com>
2021-01-05 02:55:05 +0100

    Added JavaDoc

--- src/main/java/net/tassia/calc/provider/VariableProvider.java
+++ src/main/java/net/tassia/calc/provider/VariableProvider.java
@@ -4,9 +4,20 @@ import net.tassia.calc.value.NumericValue;
 
 import java.util.NoSuchElementException;
 
+/**
+ * A variable provider is responsible for providing variables by name.
+ * @author Tassilo
+ * @since Calc 1.0
+ */
 @FunctionalInterface
 public interface VariableProvider {
 
+	/**
+	 * Returns the value of the variable with the matching name.
+	 * @param name the matching name
+	 * @return the value of the variable
+	 * @throws NoSuchElementException if no variable with the given name exists
+	 */
 	NumericValue provideVariable(String name) throws NoSuchElementException;
 
 }
