57d43c59b317e930011f2818044d08b90a705501
Tassilo <tassia710@gmail.com>
2021-01-05 03:45:06 +0100

    Added JavaDoc

--- src/main/java/net/tassia/calc/value/UnaryValue.java
+++ src/main/java/net/tassia/calc/value/UnaryValue.java
@@ -2,21 +2,39 @@ package net.tassia.calc.value;
 
 import net.tassia.calc.Operator;
 
+/**
+ * An unary value consists of an operator and a single value.
+ * @author Tassilo
+ * @since Core 1.0
+ */
 public class UnaryValue implements NumericValue {
 
     private final Operator.Unary operator;
     private final NumericValue value;
 
-    public UnaryValue(Operator.Unary operator, NumericValue value) {
+	/**
+	 * Creates a new unary value with the given operator and value.
+	 * @param operator the operator
+	 * @param value the value
+	 */
+	public UnaryValue(Operator.Unary operator, NumericValue value) {
         this.operator = operator;
         this.value = value;
     }
 
-    public Operator.Unary getOperator() {
+	/**
+	 * Returns the operator of this unary value.
+	 * @return the operator
+	 */
+	public Operator.Unary getOperator() {
         return operator;
     }
 
-    public NumericValue getValue() {
+	/**
+	 * Returns the value of this unary value.
+	 * @return the value
+	 */
+	public NumericValue getValue() {
         return value;
     }
 
