a5774dceb864bdc08bb875befc0dfe6b91b46918
Tassilo <tassia710@gmail.com>
2021-01-05 01:33:29 +0100

    Added JavaDoc

--- src/main/java/net/tassia/calculator/Calculator.java
+++ src/main/java/net/tassia/calculator/Calculator.java
@@ -3,21 +3,103 @@ package net.tassia.calculator;
 import net.tassia.calculator.parser.DefaultExpressionParser;
 import net.tassia.calculator.parser.ExpressionParser;
 import net.tassia.calculator.parser.ParseException;
-import net.tassia.calculator.provider.FunctionProvider;
 import net.tassia.calculator.provider.ListFunctionProvider;
 import net.tassia.calculator.provider.ListVariableProvider;
-import net.tassia.calculator.provider.VariableProvider;
 import net.tassia.calculator.value.NumericValue;
 
+/**
+ * This class can be used to parse and calculate expressions.
+ * @author Tassilo
+ * @since Calc 1.0
+ */
 public class Calculator {
 
-	public static double calculate(String term) throws ParseException {
-		ExpressionParser parser = new DefaultExpressionParser();
-		NumericValue value = parser.parse(term);
-		Evaluator evaluator = new DefaultEvaluator();
-		VariableProvider variableProvider = new ListVariableProvider(Variable.getDefault());
-		FunctionProvider functionProvider = new ListFunctionProvider(Function.getDefault());
+	private final ListVariableProvider variableProvider;
+	private final ListFunctionProvider functionProvider;
+	private final Evaluator evaluator;
+
+	/**
+	 * Creates a new calculator with no pre-defined variables or functions.
+	 */
+	public Calculator() {
+		this(new Variable[0], new Function[0]);
+	}
+
+	/**
+	 * Creates a new calculator with the given pre-defined variables.
+	 * @param variables pre-defined variables
+	 */
+	public Calculator(Variable...variables) {
+		this(variables, new Function[0]);
+	}
+
+	/**
+	 * Creates a new calculator with the given pre-defined functions.
+	 * @param functions pre-defined functions
+	 */
+	public Calculator(Function...functions) {
+		this(new Variable[0], functions);
+	}
+
+	/**
+	 * Creates a new calculator with the given pre-defined variables and functions.
+	 * @param variables pre-defined variables
+	 * @param functions pre-defined functions
+	 */
+	public Calculator(Variable[] variables, Function[] functions) {
+		this.variableProvider = new ListVariableProvider(variables);
+		this.functionProvider = new ListFunctionProvider(functions);
+		this.evaluator = new DefaultEvaluator();
+	}
+
+
+
+	public void addFunction(Function function) {
+		functionProvider.addFunction(function);
+	}
+
+	public void removeFunction(Function function) {
+		functionProvider.removeFunction(function.getName());
+	}
+
+	public void removeFunction(String name) {
+		functionProvider.removeFunction(name);
+	}
+
+
+
+	public void addVariable(Variable variable) {
+		variableProvider.addVariable(variable);
+	}
+
+	public void removeVariable(Variable variable) {
+		variableProvider.removeVariable(variable.getName());
+	}
+
+	public void removeVariable(String name) {
+		variableProvider.removeVariable(name);
+	}
+
+
+
+	/**
+	 * Evaluates the given numeric value to a primitive double.<br/>
+	 * <b>Note:</b> Depending on your numeric value, this method might take a while to compute.
+	 * @param value the numeric value
+	 * @return the evaluated value
+	 */
+	public double evaluateTerm(NumericValue value) {
 		return evaluator.evaluate(value, variableProvider, functionProvider);
+	}
+
+
+
+	public static Calculator newDefault() {
+		return new Calculator(Variable.getDefault(), Function.getDefault());
+	}
+
+	public static double calculate(String term) throws ParseException {
+		return Calculator.newDefault().evaluateTerm(new DefaultExpressionParser().parse(term));
 	}
 
 }
