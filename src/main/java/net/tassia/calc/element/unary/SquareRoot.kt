705c3e9c535cfc9fa9c765e907f17182ed67652f
Tassilo <tassia710@gmail.com>
2021-03-27 00:44:58 +0100

    Update SquareRoot.kt

--- src/main/java/net/tassia/calc/element/unary/SquareRoot.kt
+++ src/main/java/net/tassia/calc/element/unary/SquareRoot.kt
@@ -3,6 +3,12 @@ package net.tassia.calc.element.unary
 import net.tassia.calc.ICalculatable
 import net.tassia.calc.Math
 
+/**
+ * The square root is a unary-operation that calculates the square root of a given value.
+ *
+ * @since Calc 1.0
+ * @author Tassilo
+ */
 class SquareRoot(override val operand: ICalculatable) : IUnaryOperation, ICalculatable {
 
 	override val name: String = "sqrt"
