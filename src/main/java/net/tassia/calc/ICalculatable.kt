34dd00945731526a0f7dfb30ee6eb398d810118c
Tassilo <tassia710@gmail.com>
2021-03-26 17:47:29 +0100

    Reset!! Whoooo!!

--- src/main/java/net/tassia/calc/ICalculatable.kt
+++ src/main/java/net/tassia/calc/ICalculatable.kt
@@ -1,12 +1,29 @@
 package net.tassia.calc
 
 /**
- * Defines that something can be evaluated into an actual [Double] value.
+ * Base for essentially every element of this library.
  *
  * @since Calc 1.0
  * @author Tassilo
  */
 interface ICalculatable {
+
+	operator fun plus(other: ICalculatable): ICalculatable
+	operator fun minus(other: ICalculatable): ICalculatable
+	operator fun times(other: ICalculatable): ICalculatable
+	operator fun div(other: ICalculatable): ICalculatable
+	operator fun rem(other: ICalculatable): ICalculatable
+	// TODO: RangeTo
+
+	operator fun unaryPlus(): ICalculatable = this
+	operator fun unaryMinus(): ICalculatable
+
+	operator fun inc(): ICalculatable
+	operator fun dec(): ICalculatable
+
+	operator fun compareTo(other: ICalculatable): Int
+
+
 
 	/**
 	 * Evaluates the [Float] value for this element.
