886b3dc688e7fadb3a3eccb830dc3896de807add
Tassilo <tassia710@gmail.com>
2021-03-27 00:44:32 +0100

    Update Power.kt

--- src/main/java/net/tassia/calc/element/binary/Power.kt
+++ src/main/java/net/tassia/calc/element/binary/Power.kt
@@ -3,6 +3,12 @@ package net.tassia.calc.element.binary
 import net.tassia.calc.ICalculatable
 import net.tassia.calc.Math
 
+/**
+ * The power is a binary-operation the raises the left-hand value to the right-hand value.
+ *
+ * @since Calc 1.0
+ * @author Tassilo
+ */
 class Power(override val leftHand: ICalculatable, override val rightHand: ICalculatable) : IBinaryOperation, ICalculatable {
 
 	override val name: String = "pow"
