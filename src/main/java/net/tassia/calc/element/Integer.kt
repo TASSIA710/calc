961dfb988b22a5381c249ef5d363961f470ebcbb
Tassilo <tassia710@gmail.com>
2021-03-27 01:19:02 +0100

    Update Integer.kt

--- src/main/java/net/tassia/calc/element/Integer.kt
+++ src/main/java/net/tassia/calc/element/Integer.kt
@@ -1,9 +1,6 @@
 package net.tassia.calc.element
 
 import net.tassia.calc.ICalculatable
-import net.tassia.calc.element.binary.Fraction
-import net.tassia.calc.element.poly.Product
-import net.tassia.calc.element.poly.Sum
 
 /**
  * Represents an integer, ranging from essentially negative infinity to positive infinity
@@ -15,16 +12,6 @@ import net.tassia.calc.element.poly.Sum
  * @author Tassilo
  */
 class Integer(val value: Long) : ICalculatable {
-
-	override fun plus(other: ICalculatable): ICalculatable = Sum(this, other)
-
-	override fun minus(other: ICalculatable): ICalculatable = plus(-other)
-
-	override fun times(other: ICalculatable): ICalculatable = Product(this, other)
-
-	override fun div(other: ICalculatable): ICalculatable = Fraction(this, other)
-
-
 
 	override fun toFunctionString(): String = value.toString()
 
