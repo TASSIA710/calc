94ff3ca76a464f9559c7433171e3be8db2065c75
Tassilo <tassia710@gmail.com>
2021-03-27 01:19:06 +0100

    Update VariableCall.kt

--- src/main/java/net/tassia/calc/element/VariableCall.kt
+++ src/main/java/net/tassia/calc/element/VariableCall.kt
@@ -1,9 +1,6 @@
 package net.tassia.calc.element
 
 import net.tassia.calc.ICalculatable
-import net.tassia.calc.element.binary.Fraction
-import net.tassia.calc.element.poly.Product
-import net.tassia.calc.element.poly.Sum
 import net.tassia.calc.variable.Variable
 
 /**
@@ -15,16 +12,6 @@ import net.tassia.calc.variable.Variable
  * @author Tassilo
  */
 class VariableCall(val variable: Variable) : ICalculatable {
-
-	override fun plus(other: ICalculatable): ICalculatable = Sum(this, other)
-
-	override fun minus(other: ICalculatable): ICalculatable = plus(-other)
-
-	override fun times(other: ICalculatable): ICalculatable = Product(this, other)
-
-	override fun div(other: ICalculatable): ICalculatable = Fraction(this, other)
-
-
 
 	override fun toFunctionString(): String = variable.name
 
